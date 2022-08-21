9b0c7784e33a840972327bd08a582de191bb0b9b
vincentmegia <vincent.megia@gmail.com>
2022-08-21 21:19:08 +0800

    feat: added splash screen callbacks

--- src/Screens/Splash/index.tsx
+++ src/Screens/Splash/index.tsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 import { Image, Text, View } from 'react-native'
 import images from '../../assets'
 import styles from './style'
 
-const Splash = () => {
+interface SplashProps {
+  onInit?: (data?: any) => any
+  onComplete?: (data?: any) => any
+}
+
+const Splash = ({ onInit, onComplete }: SplashProps) => {
+  const navigation = useNavigation<any>()
+  
+  useEffect(() => {
+    if (!onInit) return
+    onInit()
+  }, [onInit])
+
+  useEffect(() => {
+    if (!onComplete) return
+    setTimeout(() => {
+      onComplete()
+      navigation.push('Home')
+    }, 5000)
+  }, [onComplete])
+
   return (
     <View style={styles.container}>
       <View style={styles.logoContainer}>
         <Image source={images.logo} style={styles.logo} />
       </View>
       <View style={styles.textContainer}>
-        <Text style={styles.text}>Copyright Stupendousware 2022.</Text>
+        <Text style={styles.text}>Copyright Stupendousware 2022.dddd</Text>
       </View>
     </View>
   )
