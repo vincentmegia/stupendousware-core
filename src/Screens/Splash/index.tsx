ffe8245260a11647779ab980cfa439fdf9f8f7be
vincentmegia <vincent.megia@gmail.com>
2022-09-24 09:49:41 +0800

    feat: logo change

--- src/Screens/Splash/index.tsx
+++ src/Screens/Splash/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
 import { Image, Text, View } from 'react-native'
 import images from '../../assets'
 import styles from './styles'
